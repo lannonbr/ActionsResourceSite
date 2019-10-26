20656247df560455a10fb7129f3855667105e8a7
Benjamin Lannon <benjamin@lannonbr.com>
2019-10-26 13:46:27 -0400

    fix webpack breaking node-fetch

--- .github/actions/changelog-pull-action/index.js
+++ .github/actions/changelog-pull-action/index.js
@@ -1,5 +1,5 @@
 const cheerio = require("cheerio")
-const fetch = require("node-fetch")
+const fetch = require("node-fetch").default
 const core = require("@actions/core")
 const faunadb = require("faunadb")
 const q = faunadb.query
@@ -9,45 +9,49 @@ require("dotenv").config()
 const client = new faunadb.Client({ secret: process.env.FAUNADB_TOKEN })
 
 // Fetch blogposts in GitHub changelog related to GitHub Actions
-const fetchBlogposts = async () => {
+const fetchBlogposts = () => {
   // Pull down page and pass it into Cheerio
-  let html = await fetch("https://github.blog/changelog/").then(resp =>
-    resp.text()
-  )
-  const $ = cheerio.load(html)
+  return new Promise(resolve => {
+    fetch("https://github.blog/changelog/")
+      .then(resp => resp.text())
+      .then(html => {
+        const $ = cheerio.load(html)
 
-  let ActionsPosts = []
+        let ActionsPosts = []
 
-  $(".post-item").each((idx, item) => {
-    // Find the text from the <time> element of a post
-    let date = item.children
-      .filter(
-        child =>
-          child.name === "a" && child.attribs.class.includes("post-item__date")
-      )[0]
-      .children.filter(child => child.name === "time")[0]
-      .firstChild.data.trim()
+        $(".post-item").each((idx, item) => {
+          // Find the text from the <time> element of a post
+          let date = item.children
+            .filter(
+              child =>
+                child.name === "a" &&
+                child.attribs.class.includes("post-item__date")
+            )[0]
+            .children.filter(child => child.name === "time")[0]
+            .firstChild.data.trim()
 
-    // Find the <a> element to link to the actual post
-    let linkElement = item.children
-      .filter(
-        child =>
-          child.name === "div" &&
-          child.attribs.class.includes("post-item__content")
-      )[0]
-      .children.filter(child => child.name === "h4")[0]
-      .children.filter(child => child.name === "a")[0]
+          // Find the <a> element to link to the actual post
+          let linkElement = item.children
+            .filter(
+              child =>
+                child.name === "div" &&
+                child.attribs.class.includes("post-item__content")
+            )[0]
+            .children.filter(child => child.name === "h4")[0]
+            .children.filter(child => child.name === "a")[0]
 
-    let href = linkElement.attribs.href
-    let title = linkElement.firstChild.data.trim()
+          let href = linkElement.attribs.href
+          let title = linkElement.firstChild.data.trim()
 
-    // Add it to an array of posts if it includes "Action" in the title
-    if (title.includes("Action")) {
-      ActionsPosts.push({ href, title, date })
-    }
+          // Add it to an array of posts if it includes "Action" in the title
+          if (title.includes("Action")) {
+            ActionsPosts.push({ href, title, date })
+          }
+        })
+
+        resolve(ActionsPosts)
+      })
   })
-
-  return ActionsPosts
 }
 
 // hit the all_changelog_entries index and get the actual documents from such
