20bbce2e99f478379568dc6707bd76f7a5fb5dca
Benjamin Lannon <benjamin@lannonbr.com>
2019-11-10 00:37:24 -0500

    update style for og-image component

--- src/printer-components/article.js
+++ src/printer-components/article.js
@@ -1,21 +1,22 @@
 import React from "react"
 
 export default props => {
-  // props.frontmatter.title
-
   return (
     <div
       style={{
         width: 1280,
         height: 720,
         color: "white",
-        background: "blue",
+        background: "linear-gradient(312deg, #2a30c7 15%, #5B8CC8 82%)",
         padding: 10,
-        border: "10px solid green",
-        fontFamily: "Lato, Arial, Helvetica, sans-serif",
+        textTransform: "uppercase",
+        boxSizing: "border-box",
+        fontFamily: "Oswald, Arial, Helvetica, sans-serif",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        fontSize: 35,
       }}
     >
       <h1>{props.frontmatter.title}</h1>
